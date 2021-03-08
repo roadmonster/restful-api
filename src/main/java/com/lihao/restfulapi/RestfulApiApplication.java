40b1e9bc81fafc423d0d4f8f9ef130231bcb0903
roadmonster <roadmonster1989@gmail.com>
2021-03-07 23:49:05 -0800

    initial push

---
+++ src/main/java/com/lihao/restfulapi/RestfulApiApplication.java
@@ -0,0 +1,14 @@
+package com.lihao.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestfulApiApplication.class, args);
+    }
+
+}
