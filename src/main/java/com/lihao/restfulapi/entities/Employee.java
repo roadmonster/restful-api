31781d0190f2f25b4553eb770ef58bd903b737c0
roadmonster <roadmonster1989@gmail.com>
2021-03-08 00:12:29 -0800

    created employee and department entity

---
+++ src/main/java/com/lihao/restfulapi/entities/Employee.java
@@ -0,0 +1,22 @@
+package com.lihao.restfulapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private Integer id;
+    private String lastName;
+
+    private String email;
+    //1 male, 0 female
+    private Integer gender;
+    private Department department;
+    private Date birth;
+
+}
